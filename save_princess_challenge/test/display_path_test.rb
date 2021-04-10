ddc919c69c66b605b4769ff74c0d36b659c0eb22
Phillip Strom <phillip.strom@gmail.com>
2021-04-10 17:16:51 -0600

    Add find_Center test

--- save_princess_challenge/test/display_path_test.rb
+++ save_princess_challenge/test/display_path_test.rb
@@ -7,4 +7,9 @@ class DisplayPathTest < Minitest::Test
     @display_path = DisplayPath.new
   end
 
+  def test_it_can_find_center
+    assert_equal [1, 1], @display_path.find_center(2)
+    assert_equal [2, 2], @display_path.find_center(5)
+  end
+
 end
