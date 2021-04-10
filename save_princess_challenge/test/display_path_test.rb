47beb62c06495e3b84bb0c536329c4cc8b361e18
Phillip Strom <phillip.strom@gmail.com>
2021-04-10 17:55:30 -0600

    Add test to find path

--- save_princess_challenge/test/display_path_test.rb
+++ save_princess_challenge/test/display_path_test.rb
@@ -20,5 +20,10 @@ class DisplayPathTest < Minitest::Test
     assert_equal [0, 4], @display_path.find_princess(5, grid2)
   end
 
-
+  def test_it_can_find_path_to_princess
+    grid = ['---', '-m-', 'p--']
+    assert_equal "DOWN\\nLEFT", @display_path.displayPathtoPrincess(grid.size, grid)
+    grid2 = ['----p', '-----', '--m--', '-----', '-----']
+    assert_equal  "UP\\nRIGHT\\nUP\\nRIGHT", @display_path.displayPathtoPrincess(grid2.size, grid2)
+  end
 end
