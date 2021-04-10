8287fb7edec6a008a0edaee3466d314a43547c34
Phillip Strom <phillip.strom@gmail.com>
2021-04-10 17:25:50 -0600

    Build find_princess method

--- save_princess_challenge/lib/display_path.rb
+++ save_princess_challenge/lib/display_path.rb
@@ -5,12 +5,24 @@ class DisplayPath
   def displayPathtoPrincess(n,grid)
     require "pry"; binding.pry
     m_x_coord, m_y_coord = find_center(n)
-
+    p_x_coord, p_y_coord = find_princess(n, grid)
     return ""
   end
 
   def find_center(n)
     [n/2, n/2]
+  end
+
+  def find_princess(n, grid)
+    if grid[0][0] == 'p'
+      [0, 0]
+    elsif grid[0][n-1] == 'p'
+      [0, n-1]
+    elsif grid[n-1][0] == 'p'
+      [n-1, 0]
+    else
+      [n-1, n-1]
+    end
   end
 
   def run
