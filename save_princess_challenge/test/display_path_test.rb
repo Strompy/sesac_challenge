cafd0bf85d8589dc53af58d0e0066ea8b2806746
Phillip Strom <phillip.strom@gmail.com>
2021-04-10 17:15:35 -0600

    Add test setup

--- save_princess_challenge/test/display_path_test.rb
+++ save_princess_challenge/test/display_path_test.rb
@@ -1,3 +1,10 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/display_path'
+
+class DisplayPathTest < Minitest::Test
+  def setup
+    @display_path = DisplayPath.new
+  end
+
+end
