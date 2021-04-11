93169a569ec18b5efdc46c4be3a00ba3ec4df401
Phillip Strom <phillip.strom@gmail.com>
2021-04-11 14:27:46 -0600

    Add methods from initial challenge for reference

--- save_princess_2/lib/bot.rb
+++ save_princess_2/lib/bot.rb
@@ -6,6 +6,42 @@ class Bot
     puts "0 0"
   end
 
+  def displayPathtoPrincess(n,grid)
+    m_y_coord, m_x_coord = find_center(n)
+    p_y_coord, p_x_coord = find_princess(n, grid)
+    moves = []
+    until m_x_coord == p_x_coord && m_y_coord == p_y_coord
+      if p_y_coord == 0
+        m_y_coord -= 1
+        moves.push 'UP'
+      else
+        m_y_coord += 1
+        moves.push 'DOWN'
+      end
+      if p_x_coord == 0
+        m_x_coord -= 1
+        moves.push 'LEFT'
+      else
+        m_x_coord += 1
+        moves.push 'RIGHT'
+      end
+    end
+    p moves.join('\n')
+  end
+
+
+  def find_princess(n, grid)
+    if grid[0][0] == 'p'
+      [0, 0]
+    elsif grid[0][n-1] == 'p'
+      [0, n-1]
+    elsif grid[n-1][0] == 'p'
+      [n-1, 0]
+    else
+      [n-1, n-1]
+    end
+  end
+
   def run
     n = gets.to_i
 
--- save_princess_2/test/bot_test.rb
+++ save_princess_2/test/bot_test.rb
@@ -7,5 +7,23 @@ class DisplayPathTest < Minitest::Test
     @bot = Bot.new
   end
 
-  
+  def test_it_can_find_center
+    assert_equal [1, 1], @display_path.find_center(2)
+    assert_equal [2, 2], @display_path.find_center(5)
+  end
+
+  def test_it_can_find_princess
+    grid = ['---', '-m-', 'p--']
+    assert_equal [2, 0], @display_path.find_princess(3, grid)
+
+    grid2 = ['----p', '-----', '--m--', '-----', '-----']
+    assert_equal [0, 4], @display_path.find_princess(5, grid2)
+  end
+
+  def test_it_can_find_path_to_princess
+    grid = ['---', '-m-', 'p--']
+    assert_equal "DOWN\\nLEFT", @display_path.displayPathtoPrincess(grid.size, grid)
+    grid2 = ['----p', '-----', '--m--', '-----', '-----']
+    assert_equal  "UP\\nRIGHT\\nUP\\nRIGHT", @display_path.displayPathtoPrincess(grid2.size, grid2)
+  end
 end
