35c239d2f8a149f691343456f0c86c5c97aaf7bc
Phillip Strom <phillip.strom@gmail.com>
2021-04-11 14:43:49 -0600

    Add test for find_princess

--- save_princess_2/test/bot_test.rb
+++ save_princess_2/test/bot_test.rb
@@ -12,19 +12,18 @@ class DisplayPathTest < Minitest::Test
   end
 
   def test_it_can_find_princess
-    skip
-    grid = ['---', '-m-', 'p--']
-    assert_equal [2, 0], @display_path.find_princess(3, grid)
+    grid = ['-----', '-----', 'p--m-', '-----', '-----']
+    assert_equal [2, 0], @bot.find_princess(grid)
 
     grid2 = ['----p', '-----', '--m--', '-----', '-----']
-    assert_equal [0, 4], @display_path.find_princess(5, grid2)
+    assert_equal [0, 4], @bot.find_princess(grid2)
   end
 
-  def test_it_can_find_path_to_princess
-    skip
-    grid = ['---', '-m-', 'p--']
-    assert_equal "DOWN\\nLEFT", @display_path.displayPathtoPrincess(grid.size, grid)
-    grid2 = ['----p', '-----', '--m--', '-----', '-----']
-    assert_equal  "UP\\nRIGHT\\nUP\\nRIGHT", @display_path.displayPathtoPrincess(grid2.size, grid2)
-  end
+  # def test_it_can_find_path_to_princess
+  #   skip
+  #   grid = ['---', '-m-', 'p--']
+  #   assert_equal "DOWN\\nLEFT", @display_path.displayPathtoPrincess(grid.size, grid)
+  #   grid2 = ['----p', '-----', '--m--', '-----', '-----']
+  #   assert_equal  "UP\\nRIGHT\\nUP\\nRIGHT", @display_path.displayPathtoPrincess(grid2.size, grid2)
+  # end
 end
