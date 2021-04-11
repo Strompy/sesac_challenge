935375f4d332fc1804c43a9c3426911261c28258
Phillip Strom <phillip.strom@gmail.com>
2021-04-11 15:05:57 -0600

    Add test for nextMove method

--- save_princess_2/test/bot_test.rb
+++ save_princess_2/test/bot_test.rb
@@ -19,11 +19,13 @@ class DisplayPathTest < Minitest::Test
     assert_equal [0, 4], @bot.find_princess(grid2)
   end
 
-  # def test_it_can_find_path_to_princess
-  #   skip
-  #   grid = ['---', '-m-', 'p--']
-  #   assert_equal "DOWN\\nLEFT", @display_path.displayPathtoPrincess(grid.size, grid)
-  #   grid2 = ['----p', '-----', '--m--', '-----', '-----']
-  #   assert_equal  "UP\\nRIGHT\\nUP\\nRIGHT", @display_path.displayPathtoPrincess(grid2.size, grid2)
-  # end
+  def test_it_can_find_next_move
+    n, r, c = 5, 2, 3
+    grid = ['-----', '-----', 'p--m-', '-----', '-----']
+    assert_equal "LEFT", @bot.nextMove(n, r, c, grid)
+
+    grid2 = ['---p', '----', '--m-', '----']
+    n, r, c = 4, 2, 2
+    assert_equal "UP", @bot.nextMove(n, r, c, grid)
+  end
 end
