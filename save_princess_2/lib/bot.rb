e4bde15b2574dce3bab6791f0917ff14bb752fcd
Phillip Strom <phillip.strom@gmail.com>
2021-04-11 14:26:00 -0600

    Set up bot class from hackerrank initial code

---
+++ save_princess_2/lib/bot.rb
@@ -0,0 +1,22 @@
+#!/bin/ruby
+class Bot
+  def initialize; end
+
+  def nextMove(n,r,c,grid)
+    puts "0 0"
+  end
+
+  def run
+    n = gets.to_i
+
+    r,c = gets.strip.split.map {|num| num.to_i}
+
+    grid = Array.new(n)
+
+    (0...n).each do |i|
+        grid[i] = gets
+    end
+
+    nextMove(n,r,c,grid)
+  end
+end
