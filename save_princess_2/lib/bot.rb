0509cfe109da4c95690a6ec6e16d7915730ada7b
Phillip Strom <phillip.strom@gmail.com>
2021-04-11 15:15:33 -0600

    Add if logic for next move

--- save_princess_2/lib/bot.rb
+++ save_princess_2/lib/bot.rb
@@ -4,7 +4,20 @@ class Bot
 
   def nextMove(n,r,c,grid)
     p_r, p_c = find_princess(grid)
-    puts "0 0"
+    # require "pry"; binding.pry
+    if r < p_r
+      r += 1
+      "DOWN"
+    elsif r > p_r
+      r -= 1
+      "UP"
+    elsif c < p_c
+      c += 1
+      "RIGHT"
+    elsif c > p_c
+      c -= 1
+      "LEFT"
+    end
   end
 
   def find_princess(grid)
@@ -17,29 +30,6 @@ class Bot
     end
     [row, col]
   end
-
-  # def displayPathtoPrincess(n,grid)
-  #   m_y_coord, m_x_coord = find_center(n)
-  #   p_y_coord, p_x_coord = find_princess(n, grid)
-  #   moves = []
-  #   until m_x_coord == p_x_coord && m_y_coord == p_y_coord
-  #     if p_y_coord == 0
-  #       m_y_coord -= 1
-  #       moves.push 'UP'
-  #     else
-  #       m_y_coord += 1
-  #       moves.push 'DOWN'
-  #     end
-  #     if p_x_coord == 0
-  #       m_x_coord -= 1
-  #       moves.push 'LEFT'
-  #     else
-  #       m_x_coord += 1
-  #       moves.push 'RIGHT'
-  #     end
-  #   end
-  #   p moves.join('\n')
-  # end
 
 
   def run
