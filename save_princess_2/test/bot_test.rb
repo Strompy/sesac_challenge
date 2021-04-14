61be9503d24f40e5cc65cca815e01dfd86982793
Phillip Strom <phillip.strom@gmail.com>
2021-04-14 17:09:57 -0600

    Rename nextMove to solve

--- save_princess_2/test/bot_test.rb
+++ save_princess_2/test/bot_test.rb
@@ -19,17 +19,17 @@ class DisplayPathTest < Minitest::Test
     assert_equal [0, 4], @bot.find_princess(grid2)
   end
 
-  def test_it_can_find_next_move
+  def test_it_can_solve
     n, r, c = 5, 2, 3
     grid = ['-----', '-----', 'p--m-', '-----', '-----']
-    assert_equal "LEFT", @bot.nextMove(n, r, c, grid)
+    assert_equal "LEFT", @bot.solve(n, r, c, grid)
 
     grid2 = ['---p', '----', '--m-', '----']
     n, r, c = 4, 2, 2
-    assert_equal "UP", @bot.nextMove(n, r, c, grid2)
+    assert_equal "UP", @bot.solve(n, r, c, grid2)
 
     grid3 = ['-----', '-----', '----p', '-m---', '-----']
     n, r, c = 5, 3, 1
-    assert_equal "UP", @bot.nextMove(n, r, c, grid3)
+    assert_equal "UP", @bot.solve(n, r, c, grid3)
   end
 end
