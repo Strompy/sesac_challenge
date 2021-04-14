2e473a346e57ff2c208f11a525267fc9f1b92bae
Phillip Strom <phillip.strom@gmail.com>
2021-04-14 17:33:59 -0600

    Rename instance to @bot

--- save_princess_challenge/test/bot_test.rb
+++ save_princess_challenge/test/bot_test.rb
@@ -4,26 +4,26 @@ require './lib/bot'
 
 class BotTest < Minitest::Test
   def setup
-    @display_path = DisplayPath.new
+    @bot = Bot.new
   end
 
   def test_it_can_find_center
-    assert_equal [1, 1], @display_path.find_center(2)
-    assert_equal [2, 2], @display_path.find_center(5)
+    assert_equal [1, 1], @bot.find_center(2)
+    assert_equal [2, 2], @bot.find_center(5)
   end
 
   def test_it_can_find_princess
     grid = ['---', '-m-', 'p--']
-    assert_equal [2, 0], @display_path.find_princess(3, grid)
+    assert_equal [2, 0], @bot.find_princess(3, grid)
 
     grid2 = ['----p', '-----', '--m--', '-----', '-----']
-    assert_equal [0, 4], @display_path.find_princess(5, grid2)
+    assert_equal [0, 4], @bot.find_princess(5, grid2)
   end
 
   def test_it_can_find_path_to_princess
     grid = ['---', '-m-', 'p--']
-    assert_equal "DOWN\\nLEFT", @display_path.displayPathtoPrincess(grid.size, grid)
+    assert_equal "DOWN\\nLEFT", @bot.displayPathtoPrincess(grid.size, grid)
     grid2 = ['----p', '-----', '--m--', '-----', '-----']
-    assert_equal  "UP\\nRIGHT\\nUP\\nRIGHT", @display_path.displayPathtoPrincess(grid2.size, grid2)
+    assert_equal  "UP\\nRIGHT\\nUP\\nRIGHT", @bot.displayPathtoPrincess(grid2.size, grid2)
   end
 end
